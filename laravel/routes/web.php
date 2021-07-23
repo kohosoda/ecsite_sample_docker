353bde0649910feb4bd5f59226f3e5d54aecad65
kohosoda <khosoda19@gmail.com>
2021-07-23 18:02:15 +0900

    modified

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,25 +15,31 @@ use Illuminate\Support\Facades\Auth;
 */
 
 // 商品一覧画面
-Route::get('/', 'ItemController@index')->name('item.index');
-Route::get('/item/{item}', 'ItemController@show')->name('item.show');
-Route::put('/item/{item}/like', 'ItemController@like')->name('item.like');
-Route::delete('/item/{item}/like', 'ItemController@unlike')->name('item.unlike');
+Route::name('item.')->group(function(){
+  Route::get('/', 'ItemController@index')->name('index');
+  Route::get('/item/{item}', 'ItemController@show')->name('show');
+  Route::put('/item/{item}/like', 'ItemController@like')->name('like');
+  Route::delete('/item/{item}/like', 'ItemController@unlike')->name('unlike');  
+});
 
 // カート画面
-Route::get('/cartitem', 'CartItemController@index')->name('cart.index');
-Route::post('/cartitem', 'CartItemController@store')->name('cart.store');
-Route::put('/cartitem/{cartItem}', 'CartItemController@update')->name('cart.update');
-Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy')->name('cart.destroy');
+Route::name('cart.')->middleware('auth')->group(function(){
+  Route::get('/cartitem', 'CartItemController@index')->name('index');
+  Route::post('/cartitem', 'CartItemController@store')->name('store');
+  Route::put('/cartitem/{cartItem}', 'CartItemController@update')->name('update');
+  Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy')->name('destroy'); 
+});
 
 // 購入処理
-Route::post('/solditem', 'SoldItemController@store')->name('soldItem.store');
+Route::post('/solditem', 'SoldItemController@store')->middleware('auth')->name('soldItem.store');
 
 // マイページ
-Route::get('/mypage', 'UserController@show')->name('mypage.show');  //登録情報を表示
-Route::post('/mypage', 'UserController@store')->name('mypage.store'); //登録情報を更新
-Route::get('/mypage/like', 'UserController@like')->name('mypage.like'); // お気に入り
-Route::get('/mypage/history', 'UserController@history')->name('mypage.history');  //購入履歴
+Route::name('mypage.')->middleware('auth')->group(function(){
+  Route::get('/mypage', 'UserController@show')->name('show');  //登録情報を表示
+  Route::post('/mypage', 'UserController@store')->name('store'); //登録情報を更新
+  Route::get('/mypage/like', 'UserController@like')->name('like'); // お気に入り
+  Route::get('/mypage/history', 'UserController@history')->name('history');  //購入履歴  
+});
 
 //認証用ルート
 Auth::routes(); 
